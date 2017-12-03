38c01f5a8bc0ccf90669c055128123c74b132475
Bahul Jain <bkj2111@columbia.edu>
2017-12-03 16:48:14 -0500

    improvements

--- src/aoc2017/Day3.scala
+++ src/aoc2017/Day3.scala
@@ -1,46 +1,51 @@
 package aoc2017
 
-import scala.collection.mutable
-import scala.util.Try
-
 object Day3 extends App {
   val in = 312051
 
 
   def solution1(x: Int): Int = {
     def box(i: Int): Int = Math.pow(2 * i + 1, 2).toInt
+
     val r = Stream.from(0).find(box(_) >= x).get
     val d = Stream.continually((r to 0 by -1) ++ (1 until r)).flatten.apply(box(r) - x)
     r + d
   }
 
+  case class Vec(x: Int, y: Int) {
+    val r: Int = Math.max(Math.abs(x), Math.abs(y))
+
+    val atBottomRightEdge: Boolean = x == r && y == -r
+
+    def neighbors = List(Vec(x + 1, y), Vec(x + 1, y + 1), Vec(x, y + 1), Vec(x - 1, y + 1), Vec(x - 1, y),
+      Vec(x - 1, y - 1), Vec(x, y - 1), Vec(x + 1, y - 1), this)
+
+    def isOutOfBoundary(dir: Vec): Boolean = Math.pow(x + dir.x, 2) + Math.pow(y + dir.y, 2) > 2 * r * r
+
+    def move(dir: Vec): Vec = Vec(x + dir.x, y + dir.y)
+  }
+
   def solution2(a: Int): Int = {
-    type Vec = (Int, Int)
-    val grid = mutable.Map[Vec, Int]((0, 0) -> 1)
-    def g(pos: Vec) = grid.getOrElse(pos, 0)
+    var (dir, grid) = (Vec(0, 1), Map[Vec, Int](Vec(0, 0) -> 1))
+
     def getSum(pos: Vec) = {
-      val (x, y) = pos
-      val sum = g((x + 1, y)) + g((x + 1, y + 1)) + g((x, y + 1)) + g((x - 1, y + 1)) + g((x - 1, y)) +
-        g((x - 1, y - 1)) + g((x, y - 1)) + g((x + 1, y - 1)) + g((x, y))
-      grid.put((x, y), sum)
+      val sum = pos.neighbors.map(grid.getOrElse(_, 0)).sum
+      grid = grid + (pos -> sum)
       sum
     }
-    var (r, dir) = (0, (0, 1))
-    def out(loc: Vec, dir: Vec, r: Int) = Math.pow(loc._1 + dir._1, 2) + Math.pow(loc._2 + dir._2, 2) > 2 * r * r
+
     def spiralNext(prev: Vec): Vec = {
-      val (x, y) = prev
-      if (x == r && y == -r) {
-        r += 1
-        (x + 1, y)
-      } else {
-        val nxt = (x + dir._1, y + dir._2)
-        if (out(nxt, dir, r))
-          dir = (- dir._2, dir._1)
+      if (prev.atBottomRightEdge)
+        prev.move(Vec(1, 0))
+      else {
+        val nxt = prev.move(dir)
+        if (nxt.isOutOfBoundary(dir))
+          dir = Vec(-dir.y, dir.x)
         nxt
       }
     }
 
-    Stream.iterate((0, 0))(spiralNext).map(getSum).find(_ > a).get
+    Stream.iterate(Vec(0, 0))(spiralNext).map(getSum).find(_ > a).get
   }
 
   println(solution1(in))
