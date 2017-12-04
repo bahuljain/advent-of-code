8f52464e004e7bcde5f72aa7f6ffdd196fee51c1
Bahul Jain <bkj2111@columbia.edu>
2017-12-04 01:23:23 -0500

    fixed print

--- src/aoc2017/Day4.scala
+++ src/aoc2017/Day4.scala
@@ -4,9 +4,7 @@ import scala.io.Source
 
 object Day4 extends App {
   val in = Source.fromFile("src/aoc2017/day4.input.txt").getLines.map(_.split("\\s+").toList).toList
-
   val p1 = in.count(pass => pass.distinct == pass)
   val p2 = in.count(pass => pass.groupBy(_.sorted).size == pass.length)
-
-  println(p1, p2)
+  println(s"part 1 - $p1\npart 2 - $p2")
 }
