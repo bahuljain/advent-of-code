09489edd16fdef08f3350dba4b2961df0252ff90
Bahul Jain <bkj2111@columbia.edu>
2017-12-04 00:42:34 -0500

    fixed part2 of day4

--- src/aoc2017/Day4.scala
+++ src/aoc2017/Day4.scala
@@ -6,7 +6,7 @@ object Day4 extends App {
   val in = Source.fromFile("src/aoc2017/day4.input.txt").getLines.map(_.split("\\s+").toList).toList
 
   val p1 = in.count(pass => pass.distinct == pass)
-  val p2 = in.count(_.combinations(2).forall(_.map(_.toSet).distinct.size == 2))
+  val p2 = in.count(pass => pass.groupBy(_.sorted).size == pass.length)
 
   println(p1, p2)
 }
