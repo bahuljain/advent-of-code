baff64c202f5f8967db802af7141a061bf27c343
Bahul Jain <bkj2111@columbia.edu>
2017-12-05 19:09:00 -0500

    added Day 5

---
+++ src/aoc2017/Day5.scala
@@ -0,0 +1,27 @@
+package aoc2017
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day5 extends App {
+  val in = Source.fromFile("src/aoc2017/day5.input.txt").getLines.map(_.toInt).toArray
+
+  def solution(arr: Array[Int], fn: (Int) => Int): Int = {
+    @tailrec
+    def getExitStep(pos: Int, steps: Int): Int = {
+      if (pos >= arr.length || pos < 0)
+        steps
+      else {
+        val jump = arr(pos)
+        arr(pos) = jump + fn(if (jump >= 3) -1 else 1)
+        getExitStep(pos +  jump, steps + 1)
+      }
+
+    }
+    getExitStep(0, 0)
+  }
+
+
+  println("part 1 - " + solution(in.clone(), Math.abs))
+  println("part 2 - " + solution(in.clone(), identity))
+}
