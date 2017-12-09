faaea81fea280650aa4aa927f9c548d5677b0219
Bahul Jain <bkj2111@columbia.edu>
2017-12-09 13:48:26 -0500

    renamed ignore to inGarbage

--- src/aoc2017/Day9.scala
+++ src/aoc2017/Day9.scala
@@ -1,20 +1,23 @@
 package aoc2017
 
+import scala.annotation.tailrec
+
 object Day9 extends App {
   val in = scala.io.Source.fromFile("src/aoc2017/day9.input.txt").mkString
 
-  def solution(s: List[Char], score: Int, garbage: Int, depth: Int, ignore: Boolean): (Int, Int) = s match {
+  @tailrec
+  def solution(s: List[Char], score: Int, garbage: Int, depth: Int, inGarbage: Boolean): (Int, Int) = s match {
     case Nil => (score, garbage)
-    case '!' :: _ :: tail => solution(tail, score, garbage, depth, ignore)
-    case '>' ::  tail if ignore => solution(tail, score, garbage, depth, ignore = false)
-    case _ :: tail if ignore => solution(tail, score, garbage + 1, depth, ignore)
-    case '<' :: tail => solution(tail, score, garbage, depth, ignore = true)
-    case '{' :: tail => solution(tail, score, garbage, depth + 1, ignore)
-    case '}' :: tail => solution(tail, score + depth, garbage, depth - 1, ignore)
-    case _ :: tail => solution(tail, score, garbage, depth, ignore)
+    case '!' :: _ :: tail => solution(tail, score, garbage, depth, inGarbage)
+    case '>' ::  tail if inGarbage => solution(tail, score, garbage, depth, inGarbage = false)
+    case _ :: tail if inGarbage => solution(tail, score, garbage + 1, depth, inGarbage)
+    case '<' :: tail => solution(tail, score, garbage, depth, inGarbage = true)
+    case '{' :: tail => solution(tail, score, garbage, depth + 1, inGarbage)
+    case '}' :: tail => solution(tail, score + depth, garbage, depth - 1, inGarbage)
+    case _ :: tail => solution(tail, score, garbage, depth, inGarbage)
   }
 
-  val (score, garbageCount) = solution(in.toList, 0, 0, 0, ignore = false)
+  val (score, garbageCount) = solution(in.toList, 0, 0, 0, inGarbage = false)
   println(s"part 1 - $score\npart 2 - $garbageCount")
 
   assert(score == 9662)
