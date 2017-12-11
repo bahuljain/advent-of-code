b6a3742631bf81e7975ab240ff89ba5f8beeee3f
Bahul Jain <bkj2111@columbia.edu>
2017-12-10 22:16:40 -0500

    day 10 added

--- src/aoc2017/Day9.scala
+++ src/aoc2017/Day9.scala
@@ -1,15 +1,17 @@
 package aoc2017
 
-import scala.annotation.tailrec
-
 object Day9 extends App {
-  val in = scala.io.Source.fromFile("src/aoc2017/day9.input.txt").mkString
+  val in = scala.io.Source.fromFile("src/aoc2017/day9.input.txt").mkString.toList
 
-  @tailrec
-  def solution(s: List[Char], score: Int, garbage: Int, depth: Int, inGarbage: Boolean): (Int, Int) = s match {
+  @scala.annotation.tailrec
+  def solution(s: List[Char],
+               score: Int,
+               garbage: Int,
+               depth: Int,
+               inGarbage: Boolean): (Int, Int) = s match {
     case Nil => (score, garbage)
     case '!' :: _ :: tail => solution(tail, score, garbage, depth, inGarbage)
-    case '>' ::  tail if inGarbage => solution(tail, score, garbage, depth, inGarbage = false)
+    case '>' :: tail if inGarbage => solution(tail, score, garbage, depth, inGarbage = false)
     case _ :: tail if inGarbage => solution(tail, score, garbage + 1, depth, inGarbage)
     case '<' :: tail => solution(tail, score, garbage, depth, inGarbage = true)
     case '{' :: tail => solution(tail, score, garbage, depth + 1, inGarbage)
@@ -17,7 +19,8 @@ object Day9 extends App {
     case _ :: tail => solution(tail, score, garbage, depth, inGarbage)
   }
 
-  val (score, garbageCount) = solution(in.toList, 0, 0, 0, inGarbage = false)
+  val (score, garbageCount) = solution(in, 0, 0, 0, inGarbage = false)
+
   println(s"part 1 - $score\npart 2 - $garbageCount")
 
   assert(score == 9662)
