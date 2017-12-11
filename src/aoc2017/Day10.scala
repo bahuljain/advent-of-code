a5dd98c78429873402e14888b9a52a519d216c73
Bahul Jain <bkj2111@columbia.edu>
2017-12-11 02:28:51 -0500

    removed use of streams from day 10

--- src/aoc2017/Day10.scala
+++ src/aoc2017/Day10.scala
@@ -3,14 +3,14 @@ package aoc2017
 object Day10 extends App {
   val in = "14,58,0,116,179,16,1,104,2,254,167,86,255,55,122,244"
 
-  def hasher(lengths: Array[Int], rounds: Int): Array[Int] = {
+  def knotHash(lengths: Array[Int], rounds: Int): Array[Int] = {
     var (pos, skip) = (0, 0)
     val seq = Array.range(0, 256)
 
     (1 to rounds) foreach { _ =>
       lengths foreach { length =>
-        Stream.continually(seq).flatten.slice(pos, pos + length).reverse.zipWithIndex foreach {
-          case (value, idx) => seq((pos + idx) % 256) = value
+        List.tabulate(length)(idx => seq((pos + idx) % 256)).reverse.zipWithIndex foreach {
+          case (value, idx) => seq.update((pos + idx) % 256, value)
         }
         pos = (pos + length + skip) % 256
         skip += 1
@@ -19,13 +19,13 @@ object Day10 extends App {
     seq
   }
 
-  val part1 = hasher(in.split(",").map(_.toInt), 1).take(2).product
+  val part1 = knotHash(in.split(",").map(_.toInt), 1).take(2).product
 
   val part2: String =
-    hasher(in.toCharArray.map(_.toInt) ++ Array(17, 31, 73, 47, 23), 64)
+    knotHash(in.toCharArray.map(_.toInt) ++ Array(17, 31, 73, 47, 23), 64)
       .grouped(16)
       .map(_.reduceLeft(_ ^ _))
-      .map(i => "%02X".format(i))
+      .map(i => "%02x".format(i))
       .mkString
 
   println(s"part 1 - $part1\npart 2 - $part2")
