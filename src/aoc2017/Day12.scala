73cb7bdd86a48dbc7963e6880af4d823b8056bfd
Bahul Jain <bkj2111@columbia.edu>
2017-12-12 02:24:22 -0500

    refactor Day 12

--- src/aoc2017/Day12.scala
+++ src/aoc2017/Day12.scala
@@ -3,14 +3,14 @@ package aoc2017
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 
-object Day12 extends App{
+object Day12 extends App {
   val line = """(\d+) <-> (.*)""".r
   val connections: Map[Int, Set[Int]] = (scala.io.Source.fromFile("src/aoc2017/day12.input.txt").getLines map {
     case line(id, connectedPrograms) => id.trim.toInt -> connectedPrograms.split(",").map(_.trim.toInt).toSet
   } toMap) withDefaultValue Set[Int]()
 
   @tailrec
-  def findConnections(programsToProcess: Queue[Int], group: Set[Int]): Set[Int] = {
+  def findConnections(programsToProcess: Queue[Int], group: Set[Int] = Set()): Set[Int] =
     if (programsToProcess.isEmpty)
       group
     else {
@@ -19,20 +19,13 @@ object Day12 extends App{
 
       findConnections(remaining.enqueue(newConnections), group + programId)
     }
-  }
 
   @tailrec
-  def computeTotalNumberOfGroups(connections: Map[Int, Set[Int]], totalGroups: Int): Int = {
-    if (connections.isEmpty)
-      totalGroups
-    else {
-      val group = findConnections(Queue(connections.head._1), Set())
-      computeTotalNumberOfGroups(connections.filterKeys(!group.contains(_)), totalGroups + 1)
-    }
-  }
+  def getGroupsCount(programs: Set[Int], groupCount: Int = 0): Int =
+    if (programs.isEmpty)
+      groupCount
+    else
+      getGroupsCount(programs.diff(findConnections(Queue(programs.head))), groupCount + 1)
 
-  val groupWithZero = findConnections(Queue(0), Set())
-  val totalNumberOfGroups = computeTotalNumberOfGroups(connections, 0)
-
-  println(s"part 1 - ${groupWithZero.size}\npart 2 - $totalNumberOfGroups")
+  println(s"part 1 - ${findConnections(Queue(0)).size}\npart 2 - ${getGroupsCount(connections.keySet)}")
 }
